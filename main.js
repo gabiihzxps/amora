f502ba99d4f998e087f48e32fc1178a8265a096c
Hellen Gabriele <142432658+gabiihzxps@users.noreply.github.com>
2024-09-23 11:21:56 -0300

    fgf

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-let nomeUsuario='Hellen,Ana Saraiva';
-alert(nomeUsuario)
+// let nomeUsuario='Hellen,Ana Saraiva';
+// alert(nomeUsuario)
+function soma(numero1,numero2){
+    return numero1+numero2;
+}
