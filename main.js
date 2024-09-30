94f6d6bcdda3531c60c9efd9392de2db1250f3fc
Hellen Gabriele <142432658+gabiihzxps@users.noreply.github.com>
2024-09-30 11:19:27 -0300

    nyt6jnty

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
-// let nomeUsuario='Hellen,Saraiva';
-// alert(nomeUsuario)
-function soma(num1,num2){
-    return num1+num2;
-}
-console.log (soma(1,2));
-console.log ( 3*3)
-function areaQuadrado
-(lado){
-    return lado**2;
-}
-console.log(areaQuadrado(6));
-let areaTriangulo= function(base,altura){
-    return(base*altura)/2;
-}
-console.log(areaTriangulo)
-let num1=parseInt(prompt('numero'));
-let num2=parseInt(prompt('numero'));
-let num3=parseInt(prompt('numero'));
-function media (x,y,z){
-    return (x+y+z 1)3;
-}
-document.write(media(num1,num2,num3));
+// // let nomeUsuario='Hellen,Saraiva';
+// // alert(nomeUsuario)
+// function soma(num1,num2){
+//     return num1+num2;
+// }
+// console.log (soma(1,2));
+// console.log ( 3*3)
+// function areaQuadrado
+// (lado){
+//     return lado**2;
+// }
+// console.log(areaQuadrado(6));
+// let areaTriangulo= function(base,altura){
+//     return(base*altura)/2;
+// }
+// console.log(areaTriangulo)
+// let num1=parseInt(prompt('numero'));
+// let num2=parseInt(prompt('numero'));
+// let num3=parseInt(prompt('numero'));
+// function media (x,y,z){
+//     return (x+y+z)/3;
+// }
+// document.write(media(num1,num2,num3));
+let estados=['Paraná','RJ'...];
